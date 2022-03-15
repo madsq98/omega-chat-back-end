5727c730a14f94788b6ea050a97a31569b6bfa79
LindholmGithub <christianlindholm@jubii.dk>
2022-03-15 10:28:09 +0100

    Added CRUD generated stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ChatroomsModule } from './chatrooms/chatrooms.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ChatroomsModule],
   controllers: [AppController],
   providers: [AppService],
 })
